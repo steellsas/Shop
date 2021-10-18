552bdde3ae297d916e8ccb65ead8f288e0716d25
steellsas <aplienius@gmail.com>
2021-10-18 18:07:15 +0300

    add views and urls

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -12,6 +13,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
 
 
 class Product(models.Model):
@@ -34,6 +39,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
 
 
 
