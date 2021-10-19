9a94cd261575722301c62d4763d59b6ba880f82d
steellsas <aplienius@gmail.com>
2021-10-19 12:22:16 +0300

    finish product_list, detail

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop'
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -118,12 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'shop/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -9,10 +9,9 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-        return render(request,'shop/product/list.html',
-                      {'category': category,
-                       'categories': categories,
-                       'products': products})
+
+    return render(request, 'shop/product/list.html',
+                  {'category': category,'categories': categories,'products': products})
 
 
 def product_detail(request, id, slug):
@@ -20,7 +19,8 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    print(product)
     return render(request,
                   'shop/product/detail.html',
                   {'product': product})
-   
+
