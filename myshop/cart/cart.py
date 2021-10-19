ae4daee0947260b9929613b4ad7b61e9fdef9ad4
steellsas <aplienius@gmail.com>
2021-10-19 23:21:17 +0300

    fix cart change quantity

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -17,6 +17,7 @@ class Cart(object):
     def add(self, product, quantity=1, override_quantity=False):
 
         product_id = str(product.id)
+
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
 
@@ -24,6 +25,8 @@ class Cart(object):
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
+
+        self.save()
 
     def save(self):
         self.session.modified = True
