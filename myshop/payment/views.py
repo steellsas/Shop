ac1008e39b718f47c888ead17d5956d7f610c8f1
steellsas <aplienius@gmail.com>
2021-10-21 12:23:17 +0300

    add payment views functions

---
+++ myshop/payment/views.py
@@ -0,0 +1,48 @@
+import braintree
+from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+from orders.models import Order
+
+
+# instantiate Braintree payment gateway
+gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
+
+
+def payment_config(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, order_id)
+    if request.method == 'POST':
+        # retrieve nonce
+        nonce = request.POST.get('payment_method_nonce', None)
+
+        # create and submit transaction
+        result = gateway.transaction.sale({
+            'amount': f'{ total_cost: .2f}',
+            'payment_method_nonce': nonce,
+            'options': {'submit_for_settlement: True'}
+        })
+        if result.is_success():
+            order.paid = True  # mark the order as paid
+            # store the unique transaction id
+            order.braintree_id = result.transaction.id
+            order.save()
+            return redirect('payment:done')
+        else:
+            return redirect('payment:canceled')
+    else:
+        client_token = gateway.client_token.generate()
+        return render(request, 'payment/process.html',{'order': order, 'client_token': client_token})
+
+
+def payment_done(request):
+    return render(request, 'payment/done.html')
+
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
+
+
+
+
+
+
